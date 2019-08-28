e65a9764c9bf8e979fa39be71f10bd1dde3fa80e
adambirse <adambirse@gmail.com>
2019-08-28 20:45:21 +0100

    eslint fix

--- client/deploy-for-local.sh
+++ client/deploy-for-local.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# remove any left over .env file and use defaults
-rm $(dirname "$0")/.env
-
 cd $(dirname "$0")
 docker stop client && docker rm client
 docker run --name client -p 3000:80 -d client
