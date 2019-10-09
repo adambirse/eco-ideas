52b399f98c1ed7808bd758ac1ca0828457f6f77a
adambirse <adambirse@gmail.com>
2019-10-09 16:31:38 +0100

    parameterised email details

--- server/src/email/email-handler.js
+++ server/src/email/email-handler.js
@@ -1,16 +1,19 @@
 const transport = require('../email/smtp');
 
+const registration_endpoint = process.env.REGISTRATION_ENDPOINT || 'http://localhost:3000/create-account';
+const sender_email = process.env.SENDER_EMAIL || 'eco-ideas.com';
+const valid_admin_email_address = process.env.VALID_ADMIN_EMAIL_ADDRESS || 'admin@eco-ideas.com';
 
 exports.sendEmail =  async (email_address,uuid) => {
 
-const registration_url = `http://localhost:3000/create-account/${uuid}`;
+const registration_url = `${registration_endpoint}/${uuid}`;
     const body = `
     <h1>Please create an account</h1>
     <p>Click <a href="${registration_url}">here<a/> to create your account and finish the registration process</p>
 `;
 
     const message = {
-        from: 'eco-ideas.com',
+        from: sender_email,
         to: email_address,
         subject: 'Please create an account',
         html: body
@@ -20,6 +23,5 @@ const registration_url = `http://localhost:3000/create-account/${uuid}`;
 };
 
 exports.checkEmail= (email_address) => {
-    //TODO more sophisticated
-    return (email_address === 'adambirse@gmail.com');
+    return (email_address === valid_admin_email_address);
 };
